a9f9f98e24ed031a9ebaddf0f78fc7e9b92e7759
Abhinav Gupta <abg@uber.com>
2015-10-19 15:51:37 -0700

    Back to development: 0.5.3

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open('README.rst') as f:
 
 setup(
     name='thriftrw',
-    version='0.5.2',
+    version='0.5.3.dev0',
     description=(
         'A library to serialize and deserialize Thrift values.'
     ),
