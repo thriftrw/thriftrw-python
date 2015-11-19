fd70784678f977c25b3fca097b4d6f7e10aa4681
Abhinav Gupta <abg@uber.com>
2015-11-19 10:04:54 -0800

    Export version number in thriftrw.__version__
    - Fix sphinx config to use that version number
    - Make setup.py read thriftrw/__init__.py
    - Configure zest releaser to update the __version__

--- thriftrw/__init__.py
+++ thriftrw/__init__.py
@@ -28,4 +28,6 @@ from __future__ import absolute_import, unicode_literals, print_function
 from .loader import load
 from .loader import install
 
+__version__ = '1.0.1.dev0'
+
 __all__ = ['load', 'install']
