4a6029b51e70d65568e42665184bc35aa578f46f
Grayson Koonce <grayson@uber.com>
2016-11-29 15:55:21 -0800

    Back to development: 1.4.1

--- thriftrw/__init__.py
+++ thriftrw/__init__.py
@@ -28,6 +28,6 @@ from __future__ import absolute_import, unicode_literals, print_function
 from .loader import load
 from .loader import install
 
-__version__ = '1.4.0'
+__version__ = '1.4.1.dev0'
 
 __all__ = ['load', 'install']
