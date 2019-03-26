4f2f71acd7a0ac716c9ea5cdcea2162aa561304a
Abhinav Gupta <abg@uber.com>
2019-03-26 11:56:04 -0700

    Back to development: 1.7.3

--- thriftrw/__init__.py
+++ thriftrw/__init__.py
@@ -28,6 +28,6 @@ from __future__ import absolute_import, unicode_literals, print_function
 from .loader import load
 from .loader import install
 
-__version__ = '1.7.2'
+__version__ = '1.7.3.dev0'
 
 __all__ = ['load', 'install']
